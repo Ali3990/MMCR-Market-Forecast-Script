991fccac065044e3f83f020683e75930c8abc297
Alan Li <61648323+Ali3990@users.noreply.github.com>
2025-09-09 14:13:03 -0700

    refactored script into a function and returns the dataframe in memory.

--- Moody's API/Moodys_API.py
+++ Moody's API/Moodys_API.py
@@ -17,9 +17,9 @@ import openpyxl
 # The id can be found in the URL of the page of the basket of mnemonics, but basket name also works:
 BASKET_NAME = "TM Forecast - Data Buffet"
 
-# provide directory to save the file:
+# provide directory to save the output file:
 target_dir = r'C:\Users\ALi\OneDrive - MMC\Desktop\MMCR\Apt Forecasts\Forecast process 2025\Data\MA Forecast Data'
-filename = r'Data Buffet - TM forecast data.xlsx'
+filename = BASKET_NAME + ".xlsx"
 
 #####
 # Setup:
@@ -63,43 +63,48 @@ def api_call(apiCommand, accKey, encKey, call_type="GET"):
 ENC_KEY = enckey
 ACC_KEY = acckey
 
+def download_basket(basket_name:str):
+    #####
+    # 1. Set Basket name at the beginning.
+    # 2. Get list of baskets.
+    # 3. Extract the basket with a given name, and save its ID for later.
+    baskets = pd.DataFrame(json.loads(api_call("baskets/", ACC_KEY, ENC_KEY).text))
+    basketId = baskets.loc[baskets["name"]==BASKET_NAME, "basketId"].item()
+    print("Basket ID: " + basketId)
+    print("Basket Name: " + BASKET_NAME)
 
-#####
-# Identify a basket to execute:
-# 2. Get list of baskets.
-# 3. Extract the basket with a given name, and save its ID for later.
-baskets = pd.DataFrame(json.loads(api_call("baskets/", ACC_KEY, ENC_KEY).text))
-basketId = baskets.loc[baskets["name"]==BASKET_NAME, "basketId"].item()
-print("Basket ID: " + basketId)
-print("Basket Name: " + BASKET_NAME)
+    # 4. Execute a particular basket using its ID.
+    # This requires that the optional argument "type" be set to "POST".
+    call = ("orders?type=baskets&action=run&id=" + basketId)
+    order = api_call(call, ACC_KEY, ENC_KEY, call_type="POST")
+    orderId = order.text[12:48]
+    print("Order ID: " + orderId)
 
-# 4. Execute a particular basket using its ID.
-# This requires that the optional argument "type" be set to "POST".
-call = ("orders?type=baskets&action=run&id=" + basketId)
-order = api_call(call, ACC_KEY, ENC_KEY, call_type="POST")
-orderId = order.text[12:48]
-print("Order ID: " + orderId)
+    #####
+    # Download the output:
+    # 5. Periodically check if the order has completed.
+    if order.status_code != 200:
+        sleep(3)
+        print("Failed! Status Code: "+ str(order.status_code))
+    else:
+        sleep(3)
+        print("Successful Order! Status Code: " + str(order.status_code))
 
-#####
-# Download the output:
-# 5. Periodically check if the order has completed.
-if order.status_code != 200:
-    sleep(3)
-    print("Failed! Status Code: "+ str(order.status_code))
-else:
-    sleep(3)
-    print("Successful Order! Status Code: " + str(order.status_code))
+    # 6. Download completed output.
+    new_call = ("orders?type=baskets&id=" + basketId)
+    get_basket = api_call(new_call, ACC_KEY, ENC_KEY)
 
-# 6. Download completed output.
-new_call = ("orders?type=baskets&id=" + basketId)
-get_basket = api_call(new_call, ACC_KEY, ENC_KEY)
+    # 7. Load Excel file directly from memory (assuming API returns .xlsx)
+    data_df = pd.read_excel(io.BytesIO(get_basket.content))
 
-# 7. Load Excel file directly from memory (assuming API returns .xlsx)
-data_df = pd.read_excel(io.BytesIO(get_basket.content))
+    # Optional: Set index, clean data if needed
+    data_df = data_df.set_index(data_df.columns[0])
+    data_df.dropna(how='all', axis=1, inplace=True)
+    data_df = data_df.loc[:, (data_df != "").any(axis=0)]
 
-# Optional: Set index, clean data if needed
-data_df = data_df.set_index(data_df.columns[0])
-data_df.dropna(how='all', axis=1, inplace=True)
-data_df = data_df.loc[:, (data_df != "").any(axis=0)]
+    data_df.to_excel(os.path.join(target_dir,filename), engine="openpyxl")
 
-data_df.to_excel(os.path.join(target_dir,filename), engine="openpyxl")
+    return data_df
+
+#test
+download_basket(filename)
